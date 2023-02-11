b1c31e7718738f4c9b0c2f33e0db9c8af6b8a5e1
Dragana05 <momirski05@gmail.com>
2023-02-11 11:53:59 +0100

    Router, Routes, Route komponente - ruta za Top50 stranicu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Top50 from './stranice/Top50';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Top50 />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Top50 />} />
+        </Routes>
+      </Router >
     </div>
   );
 }
