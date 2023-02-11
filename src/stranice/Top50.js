bb99035588c68889d018f569d85a1587e7dcce6a
Dragana05 <momirski05@gmail.com>
2023-02-11 11:43:04 +0100

    Tabela sa top 50 ocenjenih filmova

--- src/stranice/Top50.js
+++ src/stranice/Top50.js
@@ -15,14 +15,41 @@ function Top50() {
     useEffect(() => {
         fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
             .then(response => response.json())
-            .then(response => setFilmovi(response))
+            .then(response => setFilmovi(response.slice(0, 50)))
             .catch(err => console.error(err));
-
     }, []);
 
 
     return (
         <div className="top50">
+
+            <table id="tabletop50" className="table table-bordered">
+
+                <thead>
+                    <tr className="table-warning">
+                        <th>Rank</th>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Year</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {filmovi.map(film => {
+                        return (
+                            <tr className='text-white' key={film.rank}>
+                                <td>{film.rank}</td>
+                                <td>{film.title}</td>
+                                <td>{film.genre[0]}</td>
+                                <td>{film.year}</td>
+                                <td>{film.rating}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
         </div>
     )
 }
